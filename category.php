0fa85749465eabd4fd45e2bc7340b8020fd3573d
Rickard Johansson <rickard.joh@gmail.com>
2021-04-05 18:47:03 +0200

    Ugly hack

--- category.php
+++ category.php
@@ -2,7 +2,56 @@
 /**
  * @subpackage Elling_theme
  * @since 2021
- */ 
+ */
+
+
+function na_get_gallery_image_urls( $post_id ) {
+
+    $post = get_post($post_id);
+
+    // Make sure the post has a gallery in it
+    if( ! has_shortcode( $post->post_content, 'gallery' ) )
+        return;
+
+    // Retrieve all galleries of this post
+    $galleries = get_post_galleries_images( $post );
+
+    // Loop through all galleries found
+    foreach( $galleries as $gallery ) {
+
+        // Loop through each image in each gallery
+        foreach( $gallery as $image ) {
+
+            echo '<img src="'.$image.'">';
+
+        }
+
+    }
+
+ }
+
+ function get_post_block_galleries_images( $post_id ) {
+    $content = get_post_field( 'post_content', $post_id );
+    $srcs = [];
+
+    $i = -1;
+    foreach ( parse_blocks( $content ) as $block ) {
+        if ( 'core/gallery' === $block['blockName'] ) {
+            $i++;
+            $srcs[ $i ] = [];
+
+            preg_match_all( '#src=([\'"])(.+?)\1#is', $block['innerHTML'], $src, PREG_SET_ORDER );
+            if ( ! empty( $src ) ) {
+                foreach ( $src as $s ) {
+                    $srcs[ $i ][] = $s[2];
+                }
+            }
+        }
+    }
+
+    return $srcs;
+}
+
 get_header();
 get_sidebar();
 ?>
@@ -10,6 +59,12 @@ get_sidebar();
 <div class="col col-8 main-col-wrapper">
         <section id='gallery' class="gallery grid lazyContainer">
         <?php while ( have_posts() ) : the_post(); ?>
+
+            <?php
+                $gal = get_post_gallery( $post, false );
+                print_r($gal);
+                echo "</br>";
+            ?>
 
             <?php $attachments = get_posts( array(
                 'post_type' => 'attachment',
@@ -30,9 +85,9 @@ get_sidebar();
                     </a>
                 <?php endforeach; ?>
             <?php endif; ?>
-
+            <h2>test area post</h2>
+            <?php get_post_block_galleries_images($post->ID) ?> 
         <?php endwhile;?>
-
         </section>
     </div>
 </main>
