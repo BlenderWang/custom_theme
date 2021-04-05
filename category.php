84bd0de46ee33e07387de202a3ed84a42203dfd7
Rickard Johansson <rickard.joh@gmail.com>
2021-04-05 18:50:18 +0200

    hack

--- category.php
+++ category.php
@@ -9,23 +9,42 @@ get_sidebar();
 
 <div class="col col-8 main-col-wrapper">
         <section id='gallery' class="gallery grid lazyContainer">
-        <?php while ( have_posts() ) : the_post(); ?>
-
-            <?php $attachments = get_posts( array(
+        <?php while ( have_posts() ) : the_post(); 
+            $images = [];
+            $attachments = get_posts( array(
                 'post_type' => 'attachment',
                 'posts_per_page' => -1,
                 'post_parent' => $post->ID,
                 'exclude'     => get_post_thumbnail_id()
-            ) );?>
+            ) );
+            foreach ( $attachments as $attachment ) {
+                $img_url = wp_get_attachment_url( $attachment->ID );
+                array_push($images, $img_url);
+            }
+        ?>
+            
             <?php
-            if ( $attachments ) :
-                foreach ( $attachments as $attachment ) :
-                    $class = "post-attachment mime-" . sanitize_title( $attachment->post_mime_type );
-                    $img_url = wp_get_attachment_url( $attachment->ID );?>
+                if (has_block('core/gallery', $post->post_content)) {
+                    $post_blocks = parse_blocks($post->post_content);
+                    foreach( $post_blocks as $block ) {
+                        if( 'core/gallery' === $block['blockName'] ){
+                            $src = [];
+                            preg_match_all( '/data-full-url="([^"]*)"/i', $block['innerHTML'], $src ) ;
+                            $images = array_merge($images, $src[1]);
+                            $images = array_unique($images);
+                            //print_r($images);
+                        }   
+                    }
+                } 
+            ?>
+            <?php
+            if ( $images ) :
+                foreach ( $images as $image ) :
+            ?>
                     
-                    <a href="<?php echo $img_url; ?>" data-lightbox="category-images">
+                    <a href="<?php echo $image; ?>" data-lightbox="category-images">
                         <div class="grid--item">
-                            <img alt="image-<?php echo $attachment->ID; ?>" class="lazy" data-src="<?php echo $img_url ?>" width="600" height="600"/>
+                            <img alt="image-" class="lazy" data-src="<?php echo $image ?>" width="600" height="600"/>
                         </div>
                     </a>
                 <?php endforeach; ?>
